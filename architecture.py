d87662c5b9f7dc1a3d859d00586c9cf9f6bdea5a
simonsLiang <63990611+simonsLiang@users.noreply.github.com>
2022-03-11 22:43:32 +0800

    Update architecture.py

--- architecture.py
+++ architecture.py
@@ -3,64 +3,6 @@ from . import block as B
 import torch
 import numpy as np
 
-def get_sobel(in_chan, out_chan):
-    filter_x = np.array([
-        [1, 0, -1],
-        [2, 0, -2],
-        [1, 0, -1],
-    ]).astype(np.float32)
-    filter_y = np.array([
-        [1, 2, 1],
-        [0, 0, 0],
-        [-1, -2, -1],
-    ]).astype(np.float32)
-
-    filter_x = filter_x.reshape((1, 1, 3, 3))
-    filter_x = np.repeat(filter_x, in_chan, axis=1)
-    filter_x = np.repeat(filter_x, out_chan, axis=0)
-
-    filter_y = filter_y.reshape((1, 1, 3, 3))
-    filter_y = np.repeat(filter_y, in_chan, axis=1)
-    filter_y = np.repeat(filter_y, out_chan, axis=0)
-
-    filter_x = torch.from_numpy(filter_x)
-    filter_y = torch.from_numpy(filter_y)
-    filter_x = nn.Parameter(filter_x, requires_grad=False)
-    filter_y = nn.Parameter(filter_y, requires_grad=False)
-    conv_x = nn.Conv2d(in_chan, out_chan, kernel_size=3, stride=1, padding=1, bias=False)
-    conv_x.weight = filter_x
-    conv_y = nn.Conv2d(in_chan, out_chan, kernel_size=3, stride=1, padding=1, bias=False)
-    conv_y.weight = filter_y
-    sobel_x = nn.Sequential(conv_x, nn.BatchNorm2d(out_chan))
-    sobel_y = nn.Sequential(conv_y, nn.BatchNorm2d(out_chan))
-    return sobel_x, sobel_y
-
-def run_sobel(conv_x, conv_y, input):
-    g_x = conv_x(input)
-    g_y = conv_y(input)
-    g = torch.sqrt(torch.pow(g_x, 2) + torch.pow(g_y, 2))
-    return torch.sigmoid(g) * input
-
-class ERB(nn.Module):
-    def __init__(self, in_channels, out_channels):
-        super(ERB, self).__init__()
-        self.conv1 = nn.Conv2d(in_channels, out_channels, kernel_size=1, stride=1, padding=0)
-        self.conv2 = nn.Conv2d(out_channels, out_channels, kernel_size=3, stride=1, padding=1)
-        self.relu = nn.ReLU()
-        self.bn = nn.BatchNorm2d(out_channels)
-        self.conv3 = nn.Conv2d(out_channels, out_channels, kernel_size=3, stride=1, padding=1)
-
-    def forward(self, x, relu=True):
-        x = self.conv1(x)
-        res = self.conv2(x)
-        res = self.bn(res)
-        res = self.relu(res)
-        res = self.conv3(res)
-        if relu:
-            return self.relu(x + res)
-        else:
-            return x+res
-
 class IMDN(nn.Module):
     def __init__(self, in_nc=3, nf=64, num_modules=1, out_nc=3, upscale=4):
         super(IMDN, self).__init__()
