e97705595b1d3746040002bfdb64d09d1688ac99
simonsLiang <63990611+simonsLiang@users.noreply.github.com>
2022-03-19 12:39:49 +0800

    Update architecture.py

--- architecture.py
+++ architecture.py
@@ -3,7 +3,7 @@ from . import block as B
 import torch
 
 class IMDN(nn.Module):
-    def __init__(self, in_nc=3, nf=64, num_modules=2, out_nc=3, upscale=4):
+    def __init__(self, in_nc=3, nf=64, num_modules=4, out_nc=3, upscale=4):
         super(IMDN, self).__init__()
 
         self.fea_conv = B.conv_layer(in_nc, nf, kernel_size=3)
@@ -11,6 +11,8 @@ class IMDN(nn.Module):
         # IMDBs
         self.IMDB1 = B.IMDModule(in_channels=nf)
         self.IMDB2 = B.IMDModule(in_channels=nf)
+        self.IMDB3 = B.IMDModule(in_channels=nf)
+        self.IMDB4 = B.IMDModule(in_channels=nf)
 
         self.c = B.conv_block(nf * num_modules, nf, kernel_size=1, act_type='lrelu')
 
@@ -24,8 +26,10 @@ class IMDN(nn.Module):
         out_fea = self.fea_conv(input)
         out_B1 = self.IMDB1(out_fea)
         out_B2 = self.IMDB2(out_B1)
+        out_B3 = self.IMDB3(out_B2)
+        out_B4 = self.IMDB4(out_B3)
     
-        out_B = self.c(torch.cat([out_B1,out_B2], dim=1))
+        out_B = self.c(torch.cat([out_B1,out_B2,out_B3,out_B4], dim=1))
         out_lr = self.LR_conv(out_B) + out_fea
         output = self.upsampler(out_lr)
         return output
